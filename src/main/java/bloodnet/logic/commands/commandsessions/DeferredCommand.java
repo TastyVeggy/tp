bb2e82dc5b6704901de3ba40276b6525919a7bc8
Tasty <alexius2111@gmail.com>
2025-10-12 01:56:04 +0800

    DeferredCommand Class: fix javadoc punctuations

--- src/main/java/bloodnet/logic/commands/commandsessions/DeferredCommand.java
+++ src/main/java/bloodnet/logic/commands/commandsessions/DeferredCommand.java
@@ -4,15 +4,15 @@ import bloodnet.logic.commands.CommandResult;
 import bloodnet.logic.commands.exceptions.CommandException;
 
 /**
- * Represents a deferred command that can be executed later,
+ * Represents a deferred command that can be executed later.
  */
 @FunctionalInterface
 public interface DeferredCommand {
     /**
      * Executes the deferred command.
      *
-     * @return the {@link CommandResult} of executing the command
-     * @throws CommandException If an error occurs during execution of the command
+     * @return the {@link CommandResult} of executing the command.
+     * @throws CommandException If an error occurs during execution of the command.
      */
     CommandResult run() throws CommandException;
 }
